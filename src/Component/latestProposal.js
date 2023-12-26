f92f2de26b8598fcb28eba47dd7fb8c0cfd66f76
hhzule <hhzule@gmail.com>
2023-12-26 19:43:39 +0500

    wallet connect added

--- src/Component/latestProposal.js
+++ src/Component/latestProposal.js
@@ -1,25 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const latestProposal = () => {
   return (
-    
-     <div className='latestProposal'>
-      <div className='textProposal'><h3>Profits</h3></div>
-      <div className='proposalList'>
-      <div className='lsitP'>
-        <p className='sep'>SEP#10 </p>
-        <p> stablecoin allocation</p>
+    <div className="latestProposal">
+      <div className="textProposal">
+        <h3>Profits</h3>
       </div>
-      <div className='btndiv'>
-        <button>Claim</button>
+      <div className="proposalList">
+        <div className="lsitP">
+          <p className="sep">amount </p>
+          <p> stablecoin allocation : $1,444</p>
+        </div>
+        <div className="btndiv">
+          <button>Claim</button>
+        </div>
       </div>
-      </div>
+    </div>
+  );
+};
 
-
-
-     </div>
-    
-  )
-}
-
-export default latestProposal
+export default latestProposal;
