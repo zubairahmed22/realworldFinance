cff678a3d7dd047cb51a7e544082ba060856cdb1
hhzule <hhzule@gmail.com>
2023-12-26 19:28:33 +0500

    wallet connect added

--- src/Component/Header2.js
+++ src/Component/Header2.js
@@ -1,31 +1,45 @@
-import React,{useState} from 'react'
-import { Link } from 'react-router-dom'
-import "./Header2.css"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Header2.css";
+import { CustomBtn } from "../CustomBtn";
 const Header2 = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className='Header'>
-     <div className='menuHeader'>
-      <div className='leftDiv'>
-        <div className='logoDiv'>
-          <img src='assets/images/eth_logo.png'  alt='logo' className='logo1_binance'/>
+    <div className="Header">
+      <div className="menuHeader">
+        <div className="leftDiv">
+          <div className="logoDiv">
+            <img
+              src="assets/images/eth_logo.png"
+              alt="logo"
+              className="logo1_binance"
+            />
+          </div>
+        </div>
+        <div className="RightMenu">
+          <ul
+            className={isOpen ? "RightMenu_ul active" : "RightMenu_ul inactive"}
+          >
+            <li>
+              {" "}
+              <Link to="/Home2">Home</Link>
+            </li>
+            <li>
+              {" "}
+              <Link to="/admin">Admin </Link>
+            </li>
+            <li>Mine</li>
+            <button>
+              <CustomBtn />
+            </button>
+            <button>Select Language</button>
+          </ul>
+          <i class="fa fa-bars" onClick={() => setIsOpen(!isOpen)}></i>
         </div>
       </div>
-      <div className='RightMenu'>
-        <ul className={isOpen ? 'RightMenu_ul active': "RightMenu_ul inactive"}>
-         <li> <Link to="/Home2">Home</Link></li>
-         <li> <Link to="/admin">Admin </Link></li>
-          <li>Mine</li>
-          <button>Connect Wallet</button>
-          <button>Select Language</button>
-          
-        </ul>
-        <i class="fa fa-bars" onClick={() => setIsOpen(!isOpen)}></i>
-      </div>
-     </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header2
+export default Header2;
